963c345f42643b390735a3307725db24e0725328
namu627 <0627eric@naver.com>
2026-07-28 15:07:27 +0900

    refactor: 가벼운 인사 추가

--- src/main/java/com/meta/springprepare/SpringPrepareApplication.java
+++ src/main/java/com/meta/springprepare/SpringPrepareApplication.java
@@ -9,6 +9,8 @@ public class SpringPrepareApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringPrepareApplication.class, args);
         System.out.println("Hello SpringBoot!");
+        System.out.println("HI");
     }
 
 }
+
